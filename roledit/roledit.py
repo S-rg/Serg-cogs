ea7743cef172b5aa3e06c5d131871062527ec93b
= <=>
2025-08-15 05:03:20 +0530

    Testing role

--- roledit/roledit.py
+++ roledit/roledit.py
@@ -3,3 +3,9 @@ from redbot.core import commands #type: ignore
 class Roledit(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
+
+    @commands.command()
+    async def trystuff(self, ctx, roleid=0):
+        role = ctx.guild.get_role(roleid)
+
+        await ctx.send(role)
